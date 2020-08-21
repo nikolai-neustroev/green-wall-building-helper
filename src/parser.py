66db68e6507d108a74e701990fb7d24e49e30334
nikolai <nikolai.neustroev@outlook.com>
2020-08-21 19:53:34 +0900

    getting last date and contribution count for this date

--- src/parser.py
+++ src/parser.py
@@ -1,3 +1,20 @@
+from bs4 import BeautifulSoup
+
+
 class LastDate:
     """Last contribution date"""
-    pass
+    def __init__(self, html):
+        self.html = html
+
+    def get(self):
+        soup = self.open()
+        date = soup.find_all(attrs={"class": "day"})[-1]['data-date']
+        count = soup.find_all(attrs={"class": "day"})[-1]['data-count']
+        print(date)
+        print(count)
+
+    def open(self):
+        with open(self.html, "r") as f:
+            contents = f.read()
+            soup = BeautifulSoup(contents, features="html5lib")
+            return soup
--- tests/test_parser.py
+++ tests/test_parser.py
@@ -5,5 +5,5 @@ from parser import LastDate
 
 def test_last_date():
     ld = LastDate
-    ld.get_last_date()
+    ld.get()
     assert isinstance(ld.last_date, date)
