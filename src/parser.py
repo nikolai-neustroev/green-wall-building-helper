f9c674db7d69d831ff6b1cb2412c78ba98b34671
nikolai <nikolai.neustroev@outlook.com>
2020-08-24 10:54:37 +0900

    finding last date with contribution functionality is added

--- src/parser.py
+++ src/parser.py
@@ -1,10 +1,11 @@
 from datetime import datetime
 
+import numpy as np
 import requests
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, element
 
 
-class DailyData:
+class DailyDataGrabber:
     """Last contribution date"""
 
     def __init__(self, github_username):
@@ -12,16 +13,41 @@ class DailyData:
         self.date = None
         self.count = None
 
-    def get(self):
+    def get(self) -> element.ResultSet:
         soup = self.open()
-        con = soup.find_all(attrs={"class": "day"})[-1]
-        con_date = con['data-date']
-        con_count = con['data-count']
-        self.date = datetime.strptime(con_date, "%Y-%m-%d").date()
-        self.count = int(con_count)
+        con = soup.find_all(attrs={"class": "day"})
+        return con
 
-    def open(self):
+    def open(self) -> BeautifulSoup:
         url = f'https://github.com/{self.github_username}'
         webpage = requests.get(url)
         soup = BeautifulSoup(webpage.text, features="html5lib")
         return soup
+
+
+class LastDate(DailyDataGrabber):
+    def get_last(self):
+        con = self.get()
+        con_date = con[-1]['data-date']
+        con_count = con[-1]['data-count']
+        self.date = datetime.strptime(con_date, "%Y-%m-%d").date()
+        self.count = int(con_count)
+
+
+class AllDates(DailyDataGrabber):
+    def get_all(self) -> dict:
+        con = self.get()
+        dct = dict()
+        for i in con:
+            dct[i['data-date']] = int(i['data-count'])
+        return dct
+
+
+class LastDateWithContrib(AllDates):
+    def get_last_date_w_contrib(self):
+        dates = self.get_all()
+        counts = list(dates.values())
+        last_non_zero_index = np.max(np.nonzero(counts))  # TODO: what if there are no contribs for 365 days?
+        last_non_zero = list(dates.items())[last_non_zero_index]
+        self.date = datetime.strptime(last_non_zero[0], "%Y-%m-%d").date()
+        self.count = last_non_zero[1]
--- tests/test_parser.py
+++ tests/test_parser.py
@@ -2,19 +2,37 @@ from datetime import date
 
 import pytest
 
-from parser import DailyData
+from parser import LastDate, LastDateWithContrib
+
+
+username = 'nikolai-neustroev'
 
 
 @pytest.fixture
-def ld() -> DailyData:
-    ld = DailyData('nikolai-neustroev')
-    ld.get()
+def ld() -> LastDate:
+    ld = LastDate(username)
+    ld.get_last()
     return ld
 
 
-def test_daily_data_date(ld):
+@pytest.fixture
+def ldwc() -> LastDateWithContrib:
+    ldwc = LastDateWithContrib(username)
+    ldwc.get_last_date_w_contrib()
+    return ldwc
+
+
+def test_daily_data_last_date(ld):
     assert isinstance(ld.date, date)
 
 
-def test_daily_data_count(ld):
+def test_daily_data_last_count(ld):
     assert isinstance(ld.count, int)
+
+
+def test_get_last_date_w_contrib_date(ldwc):
+    assert isinstance(ldwc.date, date)
+
+
+def test_get_last_date_w_contrib_count(ldwc):
+    assert isinstance(ldwc.count, int)
