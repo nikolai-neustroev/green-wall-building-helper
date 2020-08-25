a8da2ef74d53e628a333db1413fadaecdcba5839
nikolai <nikolai.neustroev@outlook.com>
2020-08-25 21:56:25 +0900

    message creator is added

--- src/calc.py
+++ src/calc.py
@@ -4,6 +4,8 @@ from parser import LastDateWithContrib
 
 
 class Calc:
+    """Calculates the difference between today and last contribution date"""
+
     def __init__(self, github_username):
         self.github_username = github_username
         self.day_count = None
@@ -12,4 +14,25 @@ class Calc:
         last_contribution = LastDateWithContrib(self.github_username)
         last_contribution.get_last_date_w_contrib()
         today = datetime.today().date()
-        self.day_count = today - last_contribution.date
+        day_count = today - last_contribution.date
+        self.day_count = day_count.days
+
+
+class MessageCreator(Calc):
+    """Creating a message based on contribution data"""
+
+    def __init__(self, github_username):
+        super().__init__(github_username)
+        self.decision = None
+        self.message = None
+
+    def make_decision(self):
+        self.get_day_count_since_last_contribution()
+        self.decision = True if self.day_count == 0 else False  # TODO: What if calc.day_count is negative?
+
+    def get_message(self):
+        n = "n"
+        if self.decision is True:
+            self.message = f"{n} days in a row! Keep it up!"  # TODO: replace n with actual number
+        else:
+            self.message = f"{self.day_count} day(s) with no contributions."
--- tests/test_calc.py
+++ tests/test_calc.py
@@ -2,7 +2,7 @@ from datetime import timedelta
 
 import pytest
 
-from calc import Calc
+from calc import Calc, MessageCreator
 
 
 @pytest.fixture
@@ -12,5 +12,21 @@ def calc() -> Calc:
     return calc
 
 
+@pytest.fixture
+def msg_creator() -> MessageCreator:
+    msg_creator = MessageCreator('nikolai-neustroev')
+    msg_creator.make_decision()
+    msg_creator.get_message()
+    return msg_creator
+
+
 def test_get_day_count_since_last_contribution(calc):
-    assert isinstance(calc.day_count, timedelta)
+    assert isinstance(calc.day_count, int)
+
+
+def test_make_decision(msg_creator):
+    assert isinstance(msg_creator.decision, bool)
+
+
+def test_get_message(msg_creator):
+    assert isinstance(msg_creator.message, str)
