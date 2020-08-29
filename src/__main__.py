a1728b30a759b09c295f2c28f81f238146249974
nikolai <nikolai.neustroev@outlook.com>
2020-08-29 17:43:54 +0900

    scheduling is added

--- src/__main__.py
+++ src/__main__.py
@@ -1,7 +1,18 @@
+import schedule
+
 from src.calc import MessageCreator
+
+
+def job():
+    mc = MessageCreator(username)
+    mc.get_message()
+    print(mc.message)
+
 
 print("Please enter your GitHub username: ")
 username = input()
-mc = MessageCreator(username)
-mc.get_message()
-print(mc.message)
+
+schedule.every().day.at("19:00").do(job)  # TODO: add custom time
+
+while True:
+    schedule.run_pending()
