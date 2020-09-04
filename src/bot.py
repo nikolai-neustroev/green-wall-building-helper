ec5d32a3a4548418d5b43d58c05ed413008c0373
nikolai <nikolai.neustroev@outlook.com>
2020-09-04 21:53:33 +0900

    github_username now parsed from last_text

--- src/bot.py
+++ src/bot.py
@@ -5,7 +5,8 @@ from handler import Handler
 class ChatBot(Handler, MessageCreator):
     def get_username(self):
         print("What is your name?")
-        self.github_username = input()
+        self.get_last_text()
+        self.github_username = self.last_text
 
     def show_message(self):
         self.get_username()
